aaf75bec59d46574eda255e9b954ab71599e1ad9
Austinbb <austinbaumbach2@gmail.com>
2022-06-09 14:37:16 -0700

    initial commit - Dojo Survey W Model

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+// using DojoSurveyWModel.Models;
+using ViewModelFun.Models;
+
+namespace DojoSurveyWModel.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost("result")]
+    public IActionResult Result(Survey user)
+    {
+
+      return View("Result", user);
+    }
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,10 @@
+namespace ViewModelFun.Models;
+
+public class Survey
+{
+    public string Name { get; set; } = null!;
+    public string Location { get; set; } = null!;
+    public string Language { get; set; } = null!;
+    public string? Comment { get; set; }
+}
+
